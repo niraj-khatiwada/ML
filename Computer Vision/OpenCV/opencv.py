8a955a2d3be38177d05d40cf2db86fe58356cf8e
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-03 16:04:34 +0545

    Color Spaces

--- Computer Vision/OpenCV/opencv.py
+++ Computer Vision/OpenCV/opencv.py
@@ -1,9 +1,25 @@
 import cv2
-
-input_image = cv2.imread("C:/Users/niraj/Documents/ML/OpenCV/images/abraham.jpg")
+import numpy as np
+input_image = cv2.imread("C:/Users/niraj/Anaconda3/Projects/Computer Vision/OpenCV/images/abraham.jpg")
 
 # cv2.imwrite('Hello Wolrd.jpg', input_image)
-grascaled_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY )
-cv2.imwrite("Grascaled.png", grascaled_image)
+# grascaled_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY )
+# cv2.imwrite("Grascaled.png", grascaled_image)
 
 # print("Shape is ", input_image.shape)
+grascaled_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+# print(grascaled_image[0, 0])
+# print(B, G, R)
+hsv_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
+# cv2.imwrite("abraham_hsv.jpg", hsv_image)
+# print(hsv_image[0,0])
+# print(input_image.shape)
+
+B, G, R = cv2.split(input_image)
+# cv2.imwrite("Blue.jpg", B)
+# cv2.imwrite("Green.jpg", G)
+# cv2.imwrite("Red.jpg", R)
+
+merged_image = cv2.merge( [B+100, G+100, R+100])
+cv2.imwrite("Merged.jpg", merged_image)
+
