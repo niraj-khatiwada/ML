dd9d56c98dac5f72e5afc9d4078d3d7b79f96a54
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-04 17:02:52 +0545

    Live Sketch

--- Computer Vision/OpenCV/Image Manipulation/00_miniproject_live sketch_from_webcam.py
+++ Computer Vision/OpenCV/Image Manipulation/00_miniproject_live sketch_from_webcam.py
@@ -1,21 +1,19 @@
 import cv2
 
+
 def sketch(image):
-    #Convert to graysclae
+    # Convert to graysclae
     grayscaled = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    #Clean using Gaussian Blur
-    blurred = cv2.GaussianBlur(src= grayscaled, ksize= (3, 3), sigmaX= 0)
+    # Clean using Gaussian Blur
+    blurred = cv2.GaussianBlur(src=grayscaled, ksize=(3, 3), sigmaX=0)
 
-    #Detect Edge using Canny Edge Detection Algorithm
+    # Detect Edge using Canny Edge Detection Algorithm
     canny_edge = cv2.Canny(blurred, 20, 35)
 
-    #Thresholding of image
-    ret, mask = cv2.threshold(canny_edge, thresh= 10, maxval= 255, type= cv2.THRESH_BINARY_INV )
+    # Thresholding of image
+    ret, mask = cv2.threshold(canny_edge, thresh=10, maxval=255, type=cv2.THRESH_BINARY_INV)
     return mask
-
-
-
 
 
 capture = cv2.VideoCapture(0)
@@ -29,4 +27,3 @@ capture.release()
 cv2.destroyAllWindows()
 
 
-  
