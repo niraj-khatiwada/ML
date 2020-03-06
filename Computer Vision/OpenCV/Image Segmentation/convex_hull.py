c2a6c7a5e012c0029254255dcebb0969aeb737aa
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-06 20:47:57 +0545

    Shape Matching

--- Computer Vision/OpenCV/Image Segmentation/convex_hull.py
+++ Computer Vision/OpenCV/Image Segmentation/convex_hull.py
@@ -11,21 +11,26 @@ cv2.waitKey(0)
 grayscaled = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
 
 #Canny edge
-edged = cv2.Canny(grayscaled, 20, 180)
-cv2.imshow("Canny Edge", edged)
+# edged = cv2.Canny(grayscaled, 20, 180)
+# cv2.imshow("Canny Edge", edged)
+# cv2.waitKey(0)
+
+#Thresholding
+ret, threshold = cv2.threshold(grayscaled, 170, 255, cv2.THRESH_BINARY_INV)
+cv2.imshow("Threshold", threshold)
 cv2.waitKey(0)
 
 #Find Contours
-image,contours, hierarchy = cv2.findContours(image= edged, mode= cv2.RETR_LIST, method= cv2.CHAIN_APPROX_NONE)
-cv2.imshow("Contours", image)
-cv2.waitKey(0)
+image,contours, hierarchy = cv2.findContours(image= threshold, mode= cv2.RETR_LIST, method= cv2.CHAIN_APPROX_NONE)
+# cv2.imshow("Contours", image)
+# cv2.waitKey(0)
 
 #Find sorted contours first
 
 sorted_contours = sorted(contours, key= cv2.contourArea, reverse= False)
-print(sorted_contours)
+#print(sorted_contours)
 
-for i in sorted_contours:
+for i in sorted_contours[:len(sorted_contours)-1]:
     convex_hull = cv2.convexHull(i)
     cv2.drawContours(input_image, [convex_hull], 0, (0,255,0), 3)
     cv2.imshow("Convex Hull", input_image)
